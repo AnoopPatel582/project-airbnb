15ce91883b90c0efec28af68454caeefb5792477
Anoop Patel <anooppatel582@gmail.com>
2026-06-06 00:44:22 +0530

    docs: add JSDoc for wrapAsync helper in utils/wrapAsync.js

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,3 +1,6 @@
+/**
+ * Utility wrapper function to catch async errors and pass them to Express error handlers.
+ */
 module.exports=(fn)=>{
     return (req,res,next)=>{
         fn(req,res,next).catch(next);
